40c88ae70e0e1516c73e99637bcd696e24560136
Eltun İsmayılov <eltun.ismayilov@azsoftware.az>
2024-05-20 16:08:39 +0400

    fix(fix): fix

--- Uplay.Api/Controllers/v1/ReviewController.cs
+++ Uplay.Api/Controllers/v1/ReviewController.cs
@@ -1,32 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Uplay.Api.Contracts;
-using Uplay.Application.Models;
-using Uplay.Application.Models.Core.Reviews;
-using Uplay.Application.Models.Faqs;
-using Uplay.Application.Services.Reviews;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Uplay.Api.Contracts;
+//using Uplay.Application.Models;
+//using Uplay.Application.Models.Core.Reviews;
+//using Uplay.Application.Models.Faqs;
+//using Uplay.Application.Services.Reviews;
 
-namespace Uplay.Api.Controllers.v1;
+//namespace Uplay.Api.Controllers.v1;
 
-public class ReviewController : BaseController
-{
-    private readonly IReviewService _reviewService;
+//public class ReviewController : BaseController
+//{
+//    private readonly IReviewService _reviewService;
 
-    public ReviewController(IReviewService reviewService)
-    {
-        _reviewService = reviewService;
-    }
+//    public ReviewController(IReviewService reviewService)
+//    {
+//        _reviewService = reviewService;
+//    }
 
-    // [HttpGet(ApiRoutes.ReviewRoute.GetAll)]
-    // public async Task<ActionResult<FaqGetAllResponse>> GetAll([FromQuery] PaginationFilter paginationFilter, int id)
-    // {
-    //     var data = await _reviewService.GetAll(id, paginationFilter);
-    //     return Ok(data);
-    // }
-    //
-    // [HttpPost(ApiRoutes.ReviewRoute.Create)]
-    // public async Task<ActionResult<int>> Create([FromForm] SaveReviewRequest command)
-    // {
-    //     var data = await _reviewService.Create(command);
-    //     return Ok(data.Value);
-    // }
-}
+//    [HttpGet(ApiRoutes.ReviewRoute.GetAll)]
+//    public async Task<ActionResult<FaqGetAllResponse>> GetAll([FromQuery] PaginationFilter paginationFilter, int id)
+//    {
+//        var data = await _reviewService.GetAll(id, paginationFilter);
+//        return Ok(data);
+//    }
+
+//    [HttpPost(ApiRoutes.ReviewRoute.Create)]
+//    public async Task<ActionResult<int>> Create([FromForm] SaveReviewRequest command)
+//    {
+//        var data = await _reviewService.Create(command);
+//        return Ok(data.Value);
+//    }
+//}
