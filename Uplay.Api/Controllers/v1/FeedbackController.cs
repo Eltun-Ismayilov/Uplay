212787fe9fc2aa0d24b103d7848ac35763d5c6cb
Eltun İsmayılov <eltun.ismayilov@azsoftware.az>
2024-05-27 12:35:13 +0400

    fix(fix): fix

--- Uplay.Api/Controllers/v1/FeedbackController.cs
+++ Uplay.Api/Controllers/v1/FeedbackController.cs
@@ -1,32 +1,32 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Uplay.Api.Contracts;
-//using Uplay.Application.Models;
-//using Uplay.Application.Models.Core.Feedbacks;
-//using Uplay.Application.Models.Feedbacks;
-//using Uplay.Application.Services.Feedbacks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Uplay.Api.Contracts;
+using Uplay.Application.Models;
+using Uplay.Application.Models.Core.Feedbacks;
+using Uplay.Application.Models.Feedbacks;
+using Uplay.Application.Services.Feedbacks;
 
-//namespace Uplay.Api.Controllers.v1;
+namespace Uplay.Api.Controllers.v1;
 
-//public class FeedbackController : BaseController
-//{
-//    private readonly IFeedbackService _feedbackService;
+public class FeedbackController : BaseController
+{
+    private readonly IFeedbackService _feedbackService;
 
-//    public FeedbackController(IFeedbackService feedbackService)
-//    {
-//        _feedbackService = feedbackService;
-//    }
+    public FeedbackController(IFeedbackService feedbackService)
+    {
+        _feedbackService = feedbackService;
+    }
 
-//    [HttpGet(ApiRoutes.FeedbackRoute.GetAll)]
-//    public async Task<ActionResult<FeedbackGetAllResponse>> GetAll([FromQuery] PaginationFilter paginationFilter, int id)
-//    {
-//        var data = await _feedbackService.GetAll(id, paginationFilter);
-//        return Ok(data);
-//    }
+    [HttpGet(ApiRoutes.FeedbackRoute.GetAll)]
+    public async Task<ActionResult<FeedbackGetAllResponse>> GetAll([FromQuery] PaginationFilter paginationFilter, int id)
+    {
+        var data = await _feedbackService.GetAll(id, paginationFilter);
+        return Ok(data);
+    }
 
-//    [HttpPost(ApiRoutes.FeedbackRoute.Create)]
-//    public async Task<ActionResult<int>> Create([FromBody] SaveFeedbackRequest command)
-//    {
-//        var data = await _feedbackService.Create(command);
-//        return Ok(data.Value);
-//    }
-//}
+    [HttpPost(ApiRoutes.FeedbackRoute.Create)]
+    public async Task<ActionResult<int>> Create([FromBody] SaveFeedbackRequest command)
+    {
+        var data = await _feedbackService.Create(command);
+        return Ok(data.Value);
+    }
+}
