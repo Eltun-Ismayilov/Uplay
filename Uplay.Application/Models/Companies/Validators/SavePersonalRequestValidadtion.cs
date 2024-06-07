a63ac0fb66dbd1ee28dce1a0f0b9183bacdfeccb
Eltun İsmayılov <eltun.ismayilov@azsoftware.az>
2024-06-07 11:51:13 +0400

    fix(fix): fix

---
+++ Uplay.Application/Models/Companies/Validators/SavePersonalRequestValidadtion.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Uplay.Application.Models.Companies.Validators
+{
+    public class SavePersonalRequestValidadtion : BaseValidator<SavePersonalRequest>
+    {
+        public SavePersonalRequestValidadtion()
+        {
+            RuleFor(a => a.BrandName).NotNull().NotEmpty().WithMessage("BrandName is required");
+            RuleFor(a => a.Tin).NotNull().NotEmpty().WithMessage("Tin is required");
+            RuleFor(a => a.City).NotNull().NotEmpty().WithMessage("City is required");
+            RuleFor(a => a.Location).NotNull().NotEmpty().WithMessage("Location is required");
+            RuleFor(a => a.File).NotNull().NotEmpty().WithMessage("File is required");
+            RuleFor(a => a.CategoryIds).NotNull().NotEmpty().WithMessage("CategoryIds is required");
+        }
+    }
+}
