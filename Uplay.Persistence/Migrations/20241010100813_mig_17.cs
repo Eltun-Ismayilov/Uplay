067c13f2e25e7e1ede8f420bfeecba50d6ad08bc
Eltun İsmayılov <eltun.ismayilov@azsoftware.az>
2024-10-10 14:08:43 +0400

    fix(fix): fix

---
+++ Uplay.Persistence/Migrations/20241010100813_mig_17.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Uplay.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_17 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "YoutubeId",
+                schema: "PlayList",
+                table: "PlayLists",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "YoutubeId",
+                schema: "PlayList",
+                table: "PlayLists",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
